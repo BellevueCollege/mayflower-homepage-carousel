68df1d1bc9f7d5bf858e215d2d7baf408b29f0bf
Angela Vu <angela.vu@bellevuecollege.edu>
2017-11-14 12:48:20 -0800

    Fix mayflower whitespaces

--- mayflower-homepage-carousel.php
+++ mayflower-homepage-carousel.php
@@ -17,51 +17,51 @@ License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * - Generates the plugin information in the plugin admin area.
  * - Includes all of the dependencies used by the plugin
  * - Registers the plugin
- * 
-*/
+ */
 
-//Include classes
-require_once( plugin_dir_path( __FILE__ ) . 'classes/class-slider-interface.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'classes/class-admin.php' );
-require_once( plugin_dir_path( __FILE__ ) . 'classes/class-meta-boxes.php' );
+// Include classes
+require_once plugin_dir_path( __FILE__ ) . 'classes/class-slider-interface.php';
+require_once plugin_dir_path( __FILE__ ) . 'classes/class-admin.php';
+require_once plugin_dir_path( __FILE__ ) . 'classes/class-meta-boxes.php';
 
 class Mayflower_Homepage_Carousel {
 
-    public function __construct(){
-        add_action('init',  array ($this, 'bc_slider_register'));
-    }
+	public function __construct() {
+		add_action( 'init', array( $this, 'bc_slider_register' ) );
+	}
 
-    public function bc_slider_register() {
+	public function bc_slider_register() {
 		$labels = array(
-			'name' => 'Featured Slider',
-			'singular_name' => 'Slide', 
-			'add_new' => 'Add New', 'Slide',
-			'add_new_item' => 'Add New Slide',
-			'edit_item' => 'Edit Slide',
-			'new_item' => 'New Slide',
-			'all_items' => 'Slide List',
-			'view_item' => 'View Slide',
-			'search_items' => 'Search Slides',
-			'not_found' =>  'No Slides found',
+			'name'               => 'Featured Slider',
+			'singular_name'      => 'Slide',
+			'add_new'            => 'Add New',
+			'Slide',
+			'add_new_item'       => 'Add New Slide',
+			'edit_item'          => 'Edit Slide',
+			'new_item'           => 'New Slide',
+			'all_items'          => 'Slide List',
+			'view_item'          => 'View Slide',
+			'search_items'       => 'Search Slides',
+			'not_found'          => 'No Slides found',
 			'not_found_in_trash' => 'No Slides found in Trash',
-			'parent_item_colon' => '',
-			'menu_name' => 'Featured Slider'
+			'parent_item_colon'  => '',
+			'menu_name'          => 'Featured Slider',
 		);
 
-        $args = array(
-		    'labels' => $labels,
-            'public' => true,
-            'show_ui' => true,
-            'hierarchical' => true,
-            'has_archive' =>true,
-            'rewrite' => true,
+		$args = array(
+			'labels'        => $labels,
+			'public'        => true,
+			'show_ui'       => true,
+			'hierarchical'  => true,
+			'has_archive'   => true,
+			'rewrite'       => true,
 			'menu_position' => 4,
-            'supports' => array('title', 'editor', 'thumbnail', 'category', 'author', 'revisions', /*'page-attributes',*/ 'author', 'comments'),
-			'taxonomies' => array(/*'category', 'post_tag',*/) // this is IMPORTANT
-           );
+			'supports'      => array( 'title', 'editor', 'thumbnail', 'category', 'author', 'revisions', /*'page-attributes',*/ 'author', 'comments' ),
+			'taxonomies'    => array( /*'category', 'post_tag',*/ ), // this is IMPORTANT
+		);
 
-		register_post_type( 'slider' , $args );
-    }
+		register_post_type( 'slider', $args );
+	}
 }
 
 $mhcarousel = new Mayflower_Homepage_Carousel();
